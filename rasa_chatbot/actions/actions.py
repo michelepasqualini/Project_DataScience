aebc14e4fa1dd9c7897f0b80d5996c97a7a45e29
Utente <mpasqualini26@gmail.com>
2022-07-08 17:45:25 +0200

    new intents

--- rasa_chatbot/actions/actions.py
+++ rasa_chatbot/actions/actions.py
@@ -7,21 +7,34 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import requests
+import json
+
+class ActionFindInfo(Action):
+
+    def name(self) -> Text:
+        return "action_find_info"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        name=str(tracker.get_slot('country'))
+        r=requests.get(url='https://restcountries.com/v3.1/name/{}'.format(name.lower()))
+
+        if r.status_code==200:
+            data=r.json()
+            flag=list(data[0]['flags'].values())[0]
+            capital=list(data[0]['capital'][0])
+            moneta=list(data[0]['currencies'][0].values())[0]['name']
+            subregion=data[0]['subregion']
+            area=data[0]['area']
+            output='{} is a state located in {}, the area is {}, the capital is{}, the currencies is {} and you can see the flag at this link {}'
+        else:
+            output='something went wrong'
+        
+        dispatcher.utter_message(text=output)
+
+        return []
