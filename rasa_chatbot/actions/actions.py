59fc298de146a9d6aef60304b60783cc0dc39d5c
Utente <mpasqualini26@gmail.com>
2022-07-21 15:45:13 +0200

    cleaning form

--- rasa_chatbot/actions/actions.py
+++ rasa_chatbot/actions/actions.py
@@ -9,6 +9,8 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
+import math
+import datetime as dt
 
 '''
 class ActionFindInfo(Action):
@@ -74,5 +76,49 @@ class ActionSeeCleaningSchedule(Action):
             dispatcher.utter_message(text=f'We have scheduled a cleaning for {h}:{m} {suff}.') 
 
         return []
+    
+class ActionScheduleCleaning(Action):
+    
+    def name(self) -> Text:
+        return "action_schedule_cleaning"
+    
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        duration = tracker.get_slot("duration")
+        unit = tracker.get_slot("time_unit")
+        
+        if duration is None:
+            dispatcher.utter_message(text='Sure, i will send someone to your room right away.')
+            return [SlotSet("time_unit"), SlotSet("hour"), SlotSet("minute"), SlotSet("suff")]
+
+        if unit is None:
+            dispatcher.utter_message(text='Sure, sending a cleaner to your room.')
+            return [SlotSet("duration"), SlotSet("hour"), SlotSet("minute"), SlotSet("suff")]
+
+        time_tuple = dt.datetime.now().timetuple()
+        h = time_tuple[3]
+        m = time_tuple[4]
+
+        if unit == "min":
+            m += int(duration)
+            if m > 60:
+                h += int(m/60)
+                m = m % 60
+
+        elif unit == "hour":
+            h += int(duration)
+
+        h = h % 24
+        if h > 12:
+            h = h - 12
+            suff = 'PM'
+        else:
+            suff = 'AM'
+        m = "%02d" % m
+        dispatcher.utter_message(text=f'Sure, i have scheduled a cleaning for {h}:{m} {suff}.') 
+
+        return [SlotSet("duration"), SlotSet("time_unit"), SlotSet("hour", h), SlotSet("minute", m), SlotSet("suff", suff)]
 
     
